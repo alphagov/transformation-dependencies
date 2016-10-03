15d99b7ce24f2fa13ea7cb8b5ad125dabd48c458
Theodor Vararu <theo@vararu.org>
2016-10-03 11:42:32 +0100

    Hook up google sheets API to real spreadsheet

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GoogleSheetsApi from './GoogleSheetsApi'
 
-export default function App () {
-  return <h1>Hello, world!</h1>
+var SPREADSHEET_ID = process.env.SPREADSHEET_ID
+
+export default class App extends Component {
+  state = {
+    gapi: false
+  }
+
+  constructor (props) {
+    super(props)
+
+    this.handleGoogleSheetsApiReady = this.handleGoogleSheetsApiReady.bind(this)
+    this.loadSpreadsheet = this.loadSpreadsheet.bind(this)
+  }
+
+  handleGoogleSheetsApiReady (gapi) {
+    this.setState({ gapi })
+
+    this.loadSpreadsheet()
+  }
+
+  loadSpreadsheet () {
+    this.state.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: 'dependency!A1:E'
+    }).then((response) => {
+      console.log('response', response)
+    })
+  }
+
+  render () {
+    return <div>
+      <GoogleSheetsApi
+        onReady={this.handleGoogleSheetsApiReady}
+      />
+      <h1>Hello, world!</h1>
+    </div>
+  }
 }
