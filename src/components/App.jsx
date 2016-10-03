6ed684d35ac6cc6bc29262e0719f473d4de35949
Theodor Vararu <theo@vararu.org>
2016-10-03 16:58:53 +0100

    Initial spreadsheet-powered graph

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import GoogleSheetsApi from './GoogleSheetsApi'
+import ForceDirectedGraph from './ForceDirectedGraph'
 
 var SPREADSHEET_ID = process.env.SPREADSHEET_ID
 
 export default class App extends Component {
   state = {
-    gapi: false
+    gapi: false,
+    links: [],
+    organisations: [],
+    programmes: [],
+    services: []
   }
 
   constructor (props) {
@@ -24,18 +29,65 @@ export default class App extends Component {
   loadSpreadsheet () {
     this.state.gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
-      range: 'dependency!A1:E'
+      range: 'dependency!A2:F176'
     }).then((response) => {
-      console.log('response', response)
+      const dependencies = response.result.values
+      console.log('dependency', dependencies)
+      this.setState({
+        links: dependencies.map(dep => ({ source: dep[1], target: dep[3] }))
+      })
+    })
+
+    this.state.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: 'programme!A2:Q116'
+    }).then((response) => {
+      const programmes = response.result.values
+      console.log('programmes', programmes)
+      this.setState({
+        programmes: programmes.map(prog => ({ id: prog[2] }))
+      })
+    })
+
+    this.state.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: 'organisation!A2:H998'
+    }).then((response) => {
+      const organisations = response.result.values
+      console.log('organisations', organisations)
+      this.setState({
+        organisations: organisations.map(org => ({ id: org[1] }))
+      })
+    })
+
+    this.state.gapi.client.sheets.spreadsheets.values.get({
+      spreadsheetId: SPREADSHEET_ID,
+      range: 'service!A2:D101'
+    }).then((response) => {
+      const services = response.result.values
+      console.log('services', services)
+      this.setState({
+        services: services.map(org => ({ id: org[0] }))
+      })
     })
   }
 
   render () {
+    const {organisations, programmes, links, services} = this.state
+    const loading = !organisations.length || !links.length || !programmes.length || !services.length
     return <div>
       <GoogleSheetsApi
         onReady={this.handleGoogleSheetsApiReady}
       />
-      <h1>Hello, world!</h1>
+      {(loading)
+        ? <p>Loading...</p>
+        : <ForceDirectedGraph
+          width={960}
+          height={600}
+          nodes={programmes.concat(organisations).concat(services)}
+          links={links}
+        />
+      }
     </div>
   }
 }
---
+++ src/components/ForceDirectedGraph.jsx
@@ -0,0 +1,99 @@
+import * as d3 from 'd3'
+import React, { Component } from 'react'
+// import ReactFauxDOM from 'react-faux-dom'
+import PropTypes from '../propTypes'
+
+export default class ForceDirectedGraph extends Component {
+  static propTypes = {
+    height: PropTypes.number,
+    nodes: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })).isRequired,
+    links: PropTypes.arrayOf(PropTypes.shape({
+      source: PropTypes.string.isRequired,
+      target: PropTypes.string.isRequired
+    })).isRequired,
+    width: PropTypes.number
+  }
+
+  componentDidMount () {
+    const {height, width} = this.props
+    const data = {
+      nodes: this.props.nodes.slice(),
+      links: this.props.links.slice()
+    }
+
+    const svg = d3.select('#graph')
+      .attr('height', height)
+      .attr('width', width)
+
+    // const color = d3.scaleOrdinal(d3.schemeCategory20)
+
+    const simulation = d3.forceSimulation()
+      .force('link', d3.forceLink().id(d => d.id))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(width / 2, height / 2))
+
+    const link = svg.append('g')
+        .attr('class', 'links')
+      .selectAll('line')
+      .data(data.links)
+      .enter().append('line')
+
+    const node = svg.append('g')
+        .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(data.nodes)
+      .enter().append('circle')
+        .attr('r', 5)
+        .call(d3.drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended))
+
+    node.append('title')
+      .text(d => d.id)
+
+    simulation
+      .nodes(data.nodes)
+      .on('tick', ticked)
+
+    simulation.force('link')
+      .links(data.links)
+
+    function ticked () {
+      link
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y)
+
+      node
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y)
+    }
+
+    function dragstarted (d) {
+      if (!d3.event.active) { simulation.alphaTarget(0.3).restart() }
+      d.fx = d.x
+      d.fy = d.y
+    }
+
+    function dragged (d) {
+      d.fx = d3.event.x
+      d.fy = d3.event.y
+    }
+
+    function dragended (d) {
+      if (!d3.event.active) { simulation.alphaTarget(0) }
+      d.fx = null
+      d.fy = null
+    }
+  }
+
+  render () {
+    const {height, width} = this.props
+
+    return <svg id='graph' height={height} width={width} />
+  }
+}
