cccf6ff050cc193d087cadfe38c11b03fb226889
Theodor Vararu <theo@vararu.org>
2016-10-04 13:54:20 +0100

    Add node graph keys

--- src/components/GraphKey.jsx
+++ src/components/GraphKey.jsx
@@ -3,17 +3,27 @@ import React, { Component } from 'react'
 export default class GraphKey extends Component {
 
   render () {
-    return <div>
-      <h2 className='heading-medium'>Dependency types</h2>
-      <ul className='colors-key'>
-        <li><div className='policy-area' /> = Policy Area</li>
-        <li><div className='resource-sharing' /> = Resource Sharing</li>
-        <li><div className='shared-location' /> = Shared Location</li>
-        <li><div className='responsible-for' /> = Responsible For</li>
-        <li><div className='technical-integration' /> = Technical Integration</li>
-        <li><div className='data-access' /> = Data Access</li>
-        <li><div className='unknown' /> = Unknown</li>
-      </ul>
+    return <div className='grid-row'>
+      <div className='column-one-half'>
+        <h2 className='heading-medium'>Dependency types</h2>
+        <ul className='colors-key colors-key-lines'>
+          <li><div className='policy-area' /> = Policy Area</li>
+          <li><div className='resource-sharing' /> = Resource Sharing</li>
+          <li><div className='shared-location' /> = Shared Location</li>
+          <li><div className='responsible-for' /> = Responsible For</li>
+          <li><div className='technical-integration' /> = Technical Integration</li>
+          <li><div className='data-access' /> = Data Access</li>
+          <li><div className='unknown' /> = Unknown</li>
+        </ul>
+      </div>
+      <div className='column-one-half'>
+        <h2 className='heading-medium'>Node types</h2>
+        <ul className='colors-key colors-key-circles'>
+          <li><div className='organisation' /> = Organisation</li>
+          <li><div className='programme' /> = Programme</li>
+          <li><div className='service' /> = Service</li>
+        </ul>
+      </div>
     </div>
   }
 }
