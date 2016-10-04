b526d10a37e5fa8159d2054b614f752f83b827ae
Theodor Vararu <theo@vararu.org>
2016-10-04 10:58:00 +0100

    Refactor dependency fetching and loading state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,8 @@ var SPREADSHEET_ID = process.env.SPREADSHEET_ID
 export default class App extends Component {
   state = {
     gapi: false,
-    links: [],
+    dependencies: [],
+    loading: false,
     organisations: [],
     programmes: [],
     services: []
@@ -27,54 +28,73 @@ export default class App extends Component {
   }
 
   loadSpreadsheet () {
-    this.state.gapi.client.sheets.spreadsheets.values.get({
+    this.setState({ loading: true })
+
+    const promiseDependencies = this.state.gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       range: 'dependency!A2:F176'
     }).then((response) => {
       const dependencies = response.result.values
-      console.log('dependency', dependencies)
-      this.setState({
-        links: dependencies.map(dep => ({ source: dep[1], target: dep[3] }))
-      })
+      console.log('Fetched dependencies:', dependencies)
+      this.setState({ dependencies })
     })
 
-    this.state.gapi.client.sheets.spreadsheets.values.get({
+    const promiseProgrammes = this.state.gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       range: 'programme!A2:Q116'
     }).then((response) => {
       const programmes = response.result.values
-      console.log('programmes', programmes)
-      this.setState({
-        programmes: programmes.map(prog => ({ id: prog[2] }))
-      })
+      console.log('Fetched programmes:', programmes)
+      this.setState({ programmes })
     })
 
-    this.state.gapi.client.sheets.spreadsheets.values.get({
+    const promiseOrganisations = this.state.gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       range: 'organisation!A2:H998'
     }).then((response) => {
       const organisations = response.result.values
-      console.log('organisations', organisations)
-      this.setState({
-        organisations: organisations.map(org => ({ id: org[1] }))
-      })
+      console.log('Fetched organisations:', organisations)
+      this.setState({ organisations })
     })
 
-    this.state.gapi.client.sheets.spreadsheets.values.get({
+    const promiseServices = this.state.gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       range: 'service!A2:D101'
     }).then((response) => {
       const services = response.result.values
-      console.log('services', services)
-      this.setState({
-        services: services.map(org => ({ id: org[0] }))
-      })
+      console.log('Fetched services:', services)
+      this.setState({ services })
+    })
+
+    Promise.all([
+      promiseDependencies,
+      promiseProgrammes,
+      promiseOrganisations,
+      promiseServices
+    ]).then(() => {
+      this.setState({ loading: false })
     })
   }
 
+  getNodes () {
+    const {organisations, programmes, services} = this.state
+    return []
+      .concat(organisations.map(org => ({ id: org[1] })))
+      .concat(programmes.map(prog => ({ id: prog[2] })))
+      .concat(services.map(serv => ({ id: serv[0] })))
+  }
+
+  getLinks () {
+    const {dependencies} = this.state
+    return dependencies.map(dep => ({
+      source: dep[1],
+      target: dep[3],
+      dependencyType: dep[4]
+    }))
+  }
+
   render () {
-    const {organisations, programmes, links, services} = this.state
-    const loading = !organisations.length || !links.length || !programmes.length || !services.length
+    const {loading} = this.state
     return <div>
       <GoogleSheetsApi
         onReady={this.handleGoogleSheetsApiReady}
@@ -84,8 +104,8 @@ export default class App extends Component {
         : <ForceDirectedGraph
           width={960}
           height={600}
-          nodes={programmes.concat(organisations).concat(services)}
-          links={links}
+          nodes={this.getNodes()}
+          links={this.getLinks()}
         />
       }
     </div>
