3b667f61ab7b9907adf47bf2ed4a095cc6729848
Theodor Vararu <theo@vararu.org>
2016-10-05 19:26:04 +0100

    Paraphrase the dependencies in the sidebar

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
 import {curry} from 'lodash'
 
+// These colors are also defined in Application.scss
+export const getColorFromDependencyType = {
+  'unknown': '#6f777b',
+  'policy_area': '#2E358B',
+  'resource_sharing': '#D53880',
+  'shared_location': '#df3034',
+  'technical_integration': '#FFBF47',
+  'data_access': '#28A197',
+  'responsible_for': '#F47738'
+}
+
 const getLinkNodeId = (linkNode) => {
   return (typeof linkNode === 'object')
     ? linkNode.id
